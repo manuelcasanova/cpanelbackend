8c73ad48772164c79232214f5d7cc10363a74831
Manuel Casanova <manucasanova@hotmail.com>
2022-10-19 08:15:40 -0700

    Create fake database with an object and a route to display the data

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const users = require('./fake-db')
+
 app.get("/test", async (req, res) => {
  res.send('Hello!')
+})
+
+app.get("/users", async (req, res) => {
+  res.json(users)
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ fake-db.js
@@ -0,0 +1,9 @@
+const users = [
+  {
+    "username": "Manuel",
+    "email": "manucasanova@hotmail.com",
+    "password": "Password1!"
+  }
+]
+
+module.exports = users;
