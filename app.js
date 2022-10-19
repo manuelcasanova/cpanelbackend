7e05885b5cd598f2b070849ee04df57986a2d706
Manuel Casanova <manucasanova@hotmail.com>
2022-10-19 08:07:08 -0700

    Create a route and send a message

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/test", async (req, res) => {
+ res.send('Hello!')
+})
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
